11e4962a31c538ee1f5bb89353b0c130b228f88f
Brad Venghaus <investingdoc@hotmail.com>
2023-11-19 21:37:32 -0600

    starting 1

--- blackjack.py
+++ blackjack.py
@@ -2,5 +2,29 @@
 
 import random
 
-for i in range (1,10):
-    print (i)
+
+#user1choice = input ("How Much Would You Like To Bet?")
+
+deckOfCards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+
+newChoice = random.choice(deckOfCards)
+
+print (newChoice)
+
+userCards = []
+
+
+
+
+
+for i in range (2):
+    userCards.append(random.choice(deckOfCards))
+
+
+print (f'Your cards are {userCards[0]} and {userCards[1]}')
+
+
+
+
+
+
